1522796d5ce73791f8d38984a9bcf1ad83cf4b76
baynop <bayuindrawan95@gmail.com>
2021-02-11 11:53:11 +0700

    call router

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// call router
+var routes = require('./routes');
+routes(app);
+
 app.listen(3000, () => {
     console.log(`Server started on port 3000`);
 });
